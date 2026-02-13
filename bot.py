83f4cdfb98bc0812d8cb476e812bf67ab4083659
Purr404 <callmewhoosh@gmail.com>
2026-02-13 17:57:46 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -2617,7 +2617,7 @@ async def on_command_error(ctx, error):
 class Shop(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.SHOP_IMAGE_URL = "https://your-cdn.com/shop-banner.png"  # 🔁 REPLACE WITH YOUR IMAGE URL
+        self.SHOP_IMAGE_URL = "https://cdn.discordapp.com/attachments/1470664051242700800/1471797792262455306/d4387e84d53fd24697a4218a9f6924a5.png?ex=69903e02&is=698eec82&hm=2efee7a4845963b5eedc45a24a7db034df602f55238f25b5a04168f520f2d38a&"  # 🔁 REPLACE WITH YOUR IMAGE URL
 
     # -------------------------------------------------------------------------
     # LOAD PERSISTENT SHOP MESSAGES (called on bot restart)
